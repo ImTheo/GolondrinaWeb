b72fd0fa811e7580f02f35d763ae49b069b1fa54
imTheo <theomrosero@gmail.com>
2022-07-10 19:00:05 -0500

    rest branch

--- GolondrinaAPP/Index.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-header("Status: 301 Moved Permanently");
-header("Location: http://localhost/GolondrinaAPP/view/golondrina/index.php");
-exit;
-?>
-
--- GolondrinaAPP/View/Golondrina/connect_dataHabitaciones.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $name = (new MongoDB\Client('mongodb+srv://YullianaRoman:1234Yul@cluster0.5vmpd.mongodb.net/test'))->companydb->habitaciones; 
-	//$this->db = (new MongoDB\Client('mongodb+srv://YullianaRoman:1234Yul@cluster0.5vmpd.mongodb.net/test'))->crudphpmongo;
-	//$url=(new MongoDB\Client('mongodb+srv://YullianaRoman:1234Yul@cluster0.5vmpd.mongodb.net/test'));
-	//$activity = $url->companydb->actividades;
-	$activity = (new MongoDB\Client('mongodb+srv://YullianaRoman:1234Yul@cluster0.5vmpd.mongodb.net/test'))->companydb->actividades;
-?>
