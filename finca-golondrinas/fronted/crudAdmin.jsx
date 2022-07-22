522fe3c0b8c8d72902839f6a997bacbdb40e223c
STALIN BLADIMIR RIVERA VEGA <stalinrivera1980@gmail.com>
2022-07-22 00:38:48 -0500

    Change in crudAdmin

--- finca-golondrinas/fronted/crudAdmin.jsx
+++ finca-golondrinas/fronted/crudAdmin.jsx
@@ -1,13 +1,123 @@
-import './App.css';
-import React from 'react';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap"; 
 
-function crudAdmin() {
-  return (
+const data = [
 
-    <><><><nav class="navbar navbar-expand-lg bg-light">
+];
+
+class crudAdmin extends React.Component {
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      id: "",
+      name1: "",
+      email: "",
+      number:"",
+      room:"",
+      entryDate:"",
+      returnDate:"",
+      extraServices:"",
+    },
+  };
+  
+
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
+
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
+    // eslint-disable-next-line array-callback-return
+    arreglo.map((registro) => {
+      // eslint-disable-next-line eqeqeq
+      if (dato.id == registro.id) {
+        arreglo[contador].name1 = dato.name1;
+        arreglo[contador].email = dato.email;
+        arreglo[contador].number = dato.number;
+        arreglo[contador].room = dato.room;
+        arreglo[contador].entryDate=dato.entryDate;
+        arreglo[contador].returnDate=dato.returnDate;
+        arreglo[contador].extraServices=dato.extraServices;
+      }
+      contador++;
+    });
+    this.setState({ data: arreglo, modalActualizar: false });
+  };
+
+  eliminar = (dato) => {
+    var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+dato.id);
+    // eslint-disable-next-line eqeqeq
+    if (opcion == true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      // eslint-disable-next-line array-callback-return
+      arreglo.map((registro) => {
+        // eslint-disable-next-line eqeqeq
+        if (dato.id == registro.id) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
+
+  insertar= ()=>{
+    var valorNuevo= {...this.state.form};
+    valorNuevo.id=this.state.data.length+1;
+    var lista= this.state.data;
+    lista.push(valorNuevo);
+    this.setState({ modalInsertar: false, data: lista });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
+    
+    return (
+      <><>
+      <><><><nav class="navbar navbar-expand-lg bg-light">
       <div className="container-fluit">
-        <a class="navbar-brand" href="#"><h1>Administrador</h1></a>
+        <a className="navbar-brand"href="#"><h2>Administrador</h2></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -23,7 +133,7 @@ function crudAdmin() {
               <a className="nav-link" href="#books">Reservaciones</a>
             </li>
             <li className="nav-item">
-              <button className="nav-link" href="View/Golondrina/index.php">Salir</button> 
+              <a className="nav-link" href="View/Golondrina/index.php">Salir</a>
             </li>
           </ul>
         </div>
@@ -67,7 +177,7 @@ function crudAdmin() {
           <table class="table table-sm">
             <thead>
               <tr>
-                <th scope="col">Id</th>
+                <th scope="col">#</th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Capacidad-Adultos</th>
                 <th scope="col">Capacidad-Niños</th>
@@ -83,17 +193,13 @@ function crudAdmin() {
                 <td>ninos</td>
                 <td>descripcion</td>
                 <td>
-                  
-                  <button color="primary"
-                  classname="edit" href="view/Editar.php?id=<?php echo $dato[" _id
-                  >
-                    Editar
-
-                  </button>{" "}
-                  <button color = "danger" classname="edit" href="Controller/delete.php?id=<?php echo $dato[" _id
-                  >
-                    Eliminar
-                  </button>{" "}
+                    <Button
+                      color="primary"
+                      onClick={() => this.mostrarModalActualizar("id")}
+                    >
+                      Editar
+                    </Button>{" "}
+                    <Button color="danger" onClick={()=> this.eliminar("id")}>Eliminar</Button>
                 </td>
               </tr>
 
@@ -135,6 +241,7 @@ function crudAdmin() {
                 <th scope="col">#</th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Descripcion</th>
+                <th scope="col">Descripcion</th>
                 <th scope="col">Imagen</th>
                 <th scope="col">Accion</th>
               </tr>
@@ -143,20 +250,17 @@ function crudAdmin() {
               <tr>
                 <th scope="row">id</th>
                 <td>name</td>
-                <td>description</td>
+                <td>email</td>
+                <td>Descripcion</td>
                 <td>image</td>
                 <td>
-                  
-                  <button color="primary"
-                  classname="edit" href="view/Editar.php?id=<?php echo $dato[" _id
-                  >
-                    Editar
-
-                  </button>{" "}
-                  <button color = "danger" classname="edit" href="Controller/delete.php?id=<?php echo $dato[" _id
-                  >
-                    Eliminar
-                  </button>{" "}
+                    <Button
+                      color="primary"
+                      onClick={() => this.mostrarModalActualizar("id")}
+                    >
+                      Editar
+                    </Button>{" "}
+                    <Button color="danger" onClick={()=> this.eliminar("id")}>Eliminar</Button>
                 </td>
               </tr>
             </tbody>
@@ -164,52 +268,308 @@ function crudAdmin() {
           </table>
 
         </div>
-
-
-      </section></><section id="books">
-        <h1>RESERVACIONES</h1>
-          <table class="table table-sm">
+      </section></></>
+      
+      <Container>
+      <h1>RESERVACIONES</h1>
+        <br />
+          <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Crear</Button>
+          <br />
+          <br />
+          <Table>
             <thead>
               <tr>
-                <th scope="col">Nombre</th>
-                <th scope="col">Correo Electrónico</th>
-                <th scope="col">Número telefónico</th>
-                <th scope="col">Habitación</th>
-                <th scope="col">Fecha de partida</th>
-                <th scope="col">Fecha de retorno</th>
-                <th scope="col">Servicios Extras</th>
-                <th scope="col">Acciones</th>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Correo Electrónico</th>
+                <th>Número telefónico</th>
+                <th>Habitación</th>
+                <th>Fecha de partida</th>
+                <th>Fecha de retorno</th>
+                <th>Servicios Extras</th>
+                <th>Acciones</th>
               </tr>
             </thead>
+
             <tbody>
-              <tr>
-                <td>nombreCompleto</td>
-                <td>correo</td>
-                <td>telefono</td>                            
-                <td>habitacion</td>
-                <td>recreacion</td>                            
-                <td>checkin</td>
-                <td>checkout</td>
-                <td>
-                  
-                  <button color="primary"
-                  classname="edit" href="view/Editar.php?id=<?php echo $dato[" _id
-                  >
-                    Editar
-
-                  </button>{" "}
-                  <button color = "danger" classname="edit" href="Controller/delete.php?id=<?php echo $dato[" _id
-                  >
-                    Eliminar
-                  </button>{" "}
-                </td>
-              </tr>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.name1}</td>
+                  <td>{dato.email}</td>
+                  <td>{dato.number}</td>
+                  <td>{dato.room}</td>
+                  <td>{dato.entryDate}</td>
+                  <td>{dato.returnDate}</td>
+                  <th>{dato.extraServices}</th>
+                  <td>
+                    <Button
+                      color="primary"
+                      onClick={() => this.mostrarModalActualizar(dato)}
+                    >
+                      Editar
+                    </Button>{" "}
+                    <Button color="danger" onClick={()=> this.eliminar(dato)}>Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
+          </Table>
+        </Container>
 
-          </table>
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+           <div><h3>Editar Registro</h3></div>
+          </ModalHeader>
 
-      </section></>
+          <ModalBody>
+            <FormGroup>
+              <label>
+               Id:
+              </label>
+            
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Nombre: 
+              </label>
+              <input
+                className="form-control"
+                name="name1"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.name1}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+              Correo Electrónico: 
+              </label>
+              <input
+                className="form-control"
+                name="email"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.email}
+              />
+            </FormGroup>
 
-  );
+            <FormGroup>
+              <label>
+              Número telefónico: 
+              </label>
+              <input
+                className="form-control"
+                name="number"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.number}
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>
+              Habitación: 
+              </label>
+              <input
+                className="form-control"
+                name="room"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.room}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+              Fecha de partida: 
+              </label>
+              <input
+                className="form-control"
+                name="entryDate"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.entryDate}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+              Fecha de retorno: 
+              </label>
+              <input
+                className="form-control"
+                name="returnDate"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.returnDate}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+              Servicios Extras: 
+              </label>
+              <input
+                className="form-control"
+                name="extraServices"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.extraServices}
+              />
+            </FormGroup>
+
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Editar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+
+
+
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+           <div><h3>Insertar Persona</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+                Id: 
+              </label>
+              
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.data.length+1}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Nombre: 
+              </label>
+              <input
+                className="form-control"
+                name="name1"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+              Correo Electrónico: 
+              </label>
+              <input
+                className="form-control"
+                name="email"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+              Número telefónico: 
+              </label>
+              <input
+                className="form-control"
+                name="number"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+              Habitación: 
+              </label>
+              <input
+                className="form-control"
+                name="room"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+              Fecha de partida: 
+              </label>
+              <input
+                className="form-control"
+                name="entryDate"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+              Fecha de retorno: 
+              </label>
+              <input
+                className="form-control"
+                name="returnDate"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+              Servicios Extras: 
+              </label>
+              <input
+                className="form-control"
+                name="extraServices"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.insertar()}
+            >
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+      </> 
+    );
+  }
 }
 export default crudAdmin;
