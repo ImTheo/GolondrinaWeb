0257896b9d94f5dade0890e903e7e24554ec1e46
YullianaRoman <yulianaroman376@gmail.com>
2022-08-10 15:08:48 -0500

    Updadte

--- 06-Code/finca-golondrinas/fronted/src/App.js
+++ 06-Code/finca-golondrinas/fronted/src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
-import {Homepage} from "./components/Homepage";
-import {CrudAdmin as Admin} from "./components/CrudAdmin";
-
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./components/Login";
+import { ReservationProvider } from "./controller/reservationController";
+import { ActivityProvider } from "./controller/activityController";
+import { RoomProvider } from "./controller/roomController";
+import Admin from "./components/Admin";
+import Room from "./components/Room";
+import Reservation from "./components/Reservation";
+import {Toaster} from "react-hot-toast";
 import './App.css';
 
 function App() {
   return (
-
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Homepage />} />
-    <Route path="admin/*" element={<Admin />} />
-  </Routes>
-  </BrowserRouter>
-
+    <RoomProvider>
+      <ActivityProvider>
+        <ReservationProvider>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/admin' element={<Admin />} />
+            <Route path='/room' element={<Room/>}/>             
+            <Route path='/reservation' element={<Reservation/>} />
+          </Routes>
+          <Toaster/>
+        </ReservationProvider>
+      </ActivityProvider>
+    </RoomProvider>
   );
 }
 
